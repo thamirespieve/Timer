2e83c4198a023e4e05e596899742555b2d45a8cb
Thamires Pieve <thamires.pieve@gec.inatel.br>
2022-06-30 21:33:38 -0300

    feat: sound effect

--- note.md
+++ note.md
@@ -10,19 +10,40 @@ Criar um evento de clique para os botões OK
 
 Pegar os elementos
 
-- florestSound
-- rainSound
-- coffeSound
-- fireSound
-  Criar um novo audio para cada botão de som
+- florestSound OK
+- rainSound OK
+- coffeSound OK
+- fireSound OK
+  Criar um novo audio para cada botão de som OK
 
 Criar um evento de clique para cada elemento de som
 
-- em cada evento colocar a classe selectSound
-- remover a classe selectSound dos demais sons
-- Dar play ou pause no audio
+- em cada evento colocar a classe selectSound Ok
+- remover a classe selectSound dos demais sons OK
+- Dar play ou pause no audio OK
+- Pegar o valor que está no slider e passar esse volume para o objeto de audio entre 0 e 1K
 
-Pegar os sliders dos botões de sons
+Pegar os sliders dos botões de sons OK
 
-- verificar o valor em que ele se encontra.
-- aumentar ou diminuir o audio de acordo com o valor do slider
+- verificar o valor em que ele se encontra. OK
+- aumentar ou diminuir o audio de acordo com o valor do slider OK
+
+Pegos os elementos
+
+- play
+- pause
+- addition
+- subtraction
+- minutes
+- seconds
+
+Criar um evento de click para o addition
+
+- Pegar os minutes e somar + 5 quando o evento for acionado
+- Deixar os minutos sempre com 2 casas
+
+Criar um evento de click para o substraction
+
+- Pegar os minutes e diminuir 5 quando o evento for acionado
+- Deixar os minutos sempre com 2 casas
+- Verificar se a conta não da negativa se sim manter em 0
--- script.js
+++ script.js
@@ -1,8 +1,21 @@
 const body = document.querySelector('body')
-const content = document.querySelector('.content')
-
 const lightButton = document.querySelector('.light')
 const darkButton = document.querySelector('.dark')
+
+const florestButton = document.querySelector('.florestSound')
+const rainButton = document.querySelector('.rainSound')
+const coffeButton = document.querySelector('.coffeSound')
+const fireButton = document.querySelector('.fireSound')
+
+let volume
+const florestVol = document.querySelector('#florestVol')
+const florestSound = new Audio('assests/sound/Floresta.wav')
+const rainVol = document.querySelector('#rainVol')
+const rainSound = new Audio('assests/sound/Chuva.wav')
+const coffeVol = document.querySelector('#coffetVol')
+const coffeSound = new Audio('assests/sound/Cafeteria.wav')
+const fireVol = document.querySelector('#fireVol')
+const fireSound = new Audio('assests/sound/Lareira.wav')
 
 function pageTheme() {
   lightButton.classList.toggle('noSelectTheme')
@@ -11,5 +24,65 @@ function pageTheme() {
   body.classList.toggle('darkTheme')
 }
 
+function stopMusic() {
+  florestSound.pause()
+  rainSound.pause()
+  coffeSound.pause()
+  fireSound.pause()
+}
+
 lightButton.addEventListener('click', pageTheme)
 darkButton.addEventListener('click', pageTheme)
+
+florestButton.addEventListener('click', function () {
+  florestButton.classList.add('selectSound')
+  rainButton.classList.remove('selectSound')
+  coffeButton.classList.remove('selectSound')
+  fireButton.classList.remove('selectSound')
+
+  volume = florestVol.value / 100
+
+  stopMusic()
+  florestSound.play()
+  florestSound.volume = volume
+})
+
+rainButton.addEventListener('click', function () {
+  rainButton.classList.add('selectSound')
+  florestButton.classList.remove('selectSound')
+  coffeButton.classList.remove('selectSound')
+  fireButton.classList.remove('selectSound')
+
+  volume = rainVol.value / 100
+
+  stopMusic()
+  rainSound.play()
+  rainSound.volume = volume
+})
+
+coffeButton.addEventListener('click', function () {
+  coffeButton.classList.add('selectSound')
+  florestButton.classList.remove('selectSound')
+  rainButton.classList.remove('selectSound')
+  fireButton.classList.remove('selectSound')
+
+  volume = coffeVol.value / 100
+
+  stopMusic()
+
+  coffeSound.play()
+  coffeSound.volume = volume
+})
+
+fireButton.addEventListener('click', function () {
+  fireButton.classList.add('selectSound')
+  florestButton.classList.remove('selectSound')
+  rainButton.classList.remove('selectSound')
+  coffeButton.classList.remove('selectSound')
+
+  volume = fireVol.value / 100
+
+  stopMusic()
+  fireSound.play()
+  fireSound.volume = volume
+})
